a6b0cb1e6eb582aa971d5b5d261579018d65d04e
mtayyab-10 <mtayyabams@gmail.com>
2026-07-12 22:02:54 +0500

    Fix settings panel background in light mode and bump cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zenclox-v17';
+const CACHE = 'zenclox-v18';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './features.js', './manifest.json', './favicon.png'];
 
 self.addEventListener('install', e => {
